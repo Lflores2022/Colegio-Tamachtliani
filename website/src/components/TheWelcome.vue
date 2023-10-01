67235685b62425981564a601af9e2a6f280529be
Lflores2022 <lflores@comercialmm.com>
2023-09-30 21:14:10 -0600

    pagina de inicio

--- website/src/components/TheWelcome.vue
+++ website/src/components/TheWelcome.vue
@@ -1,88 +1,63 @@
-<script setup>
-import WelcomeItem from './WelcomeItem.vue'
-import DocumentationIcon from './icons/IconDocumentation.vue'
-import ToolingIcon from './icons/IconTooling.vue'
-import EcosystemIcon from './icons/IconEcosystem.vue'
-import CommunityIcon from './icons/IconCommunity.vue'
-import SupportIcon from './icons/IconSupport.vue'
-</script>
 
 <template>
-  <WelcomeItem>
-    <template #icon>
-      <DocumentationIcon />
-    </template>
-    <template #heading>Documentation</template>
-
-    Vue’s
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
-    provides you with all information you need to get started.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <ToolingIcon />
-    </template>
-    <template #heading>Tooling</template>
-
-    This project is served and bundled with
-    <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The
-    recommended IDE setup is
-    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a> +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener">Volar</a>. If
-    you need to test your components and web pages, check out
-    <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a> and
-    <a href="https://on.cypress.io/component" target="_blank" rel="noopener"
-      >Cypress Component Testing</a
-    >.
-
-    <br />
-
-    More instructions are available in <code>README.md</code>.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <EcosystemIcon />
-    </template>
-    <template #heading>Ecosystem</template>
-
-    Get official tools and libraries for your project:
-    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
-    <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
-    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener">Vue Test Utils</a>, and
-    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>. If
-    you need more resources, we suggest paying
-    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
-    a visit.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <CommunityIcon />
-    </template>
-    <template #heading>Community</template>
-
-    Got stuck? Ask your question on
-    <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>, our official
-    Discord server, or
-    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
-      >StackOverflow</a
-    >. You should also subscribe to
-    <a href="https://news.vuejs.org" target="_blank" rel="noopener">our mailing list</a> and follow
-    the official
-    <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
-    twitter account for latest news in the Vue world.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <SupportIcon />
-    </template>
-    <template #heading>Support Vue</template>
-
-    As an independent project, Vue relies on community backing for its sustainability. You can help
-    us by
-    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
-  </WelcomeItem>
+  <div class="container">
+    <div class="container__text">
+      <h2>Quienes Somos?</h2>
+      <hr />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis itaque consectetur provident, quam reiciendis,
+        earum deleniti est amet veniam praesentium dicta aliquid eaque fuga quas? Eos eius assumenda adipisci voluptate.
+      </p>
+    </div>
+    <div class="container__text">
+      <h2>Mision</h2>
+      <hr />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis itaque consectetur provident, quam reiciendis,
+        earum deleniti est amet veniam praesentium dicta aliquid eaque fuga quas? Eos eius assumenda adipisci voluptate.
+      </p>
+    </div>
+    <div class="container__text">
+      <h2>Vision</h2>
+      <hr />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis itaque consectetur provident, quam reiciendis,
+        earum deleniti est amet veniam praesentium dicta aliquid eaque fuga quas? Eos eius assumenda adipisci voluptate.
+      </p>
+    </div>
+    <div class="container__text">
+      <h2>Objetivo</h2>
+      <hr />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis itaque consectetur provident, quam reiciendis,
+        earum deleniti est amet veniam praesentium dicta aliquid eaque fuga quas? Eos eius assumenda adipisci voluptate.
+      </p>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  width: 100%;
+}
+
+.container__text {
+  background-color: rgb(255, 255, 255);
+  text-align: center;
+  width: 45%;
+}
+
+h2 {
+  margin: 0;
+  line-height: 1;
+  font-size: 3.1em;
+  letter-spacing: -1px;
+  padding-top: 30vh;
+}
+
+
+hr {
+  margin-top: 1rem;
+  height: 1px;
+  background-color: white;
+  margin-left: 0;
+}</style>
