1c4f8ceedddceedfc20bf29077916b57f9c0c604
StephanieAlferezSalcido <128555593+StephanieAlferezSalcido@users.noreply.github.com>
2023-10-11 16:01:18 -0600

    TheWelcome.vue

--- website/src/components/TheWelcome.vue
+++ website/src/components/TheWelcome.vue
@@ -45,6 +45,15 @@ Nuestra visión es ser reconocidos como un referente en educación que nutre el 
       </p>
     </div>
   </div>
+
+  <footer class="footer">
+      <img class="footer-logo" src="logo.png" alt="Logo de la empresa"> Colegio Tlamachtiani
+      <div class="footer-links">
+        <a href="#">Aviso legal</a>
+        <a href="#">Política de privacidad</a>
+        <a href="#">Términos y condiciones</a>
+      </div>
+    </footer>
 </template>
 
 <style scoped>
@@ -73,4 +82,20 @@ h2 {
 img{
   width: 60%;
 }
+
+.footer {
+        background-color: #000000;
+        padding: 20px;
+        text-align: center;
+        color: #fff;
+      }
+      .footer-logo {
+        width: 10px;
+      }
+    
+      .footer-links a {
+        color: #f3efef;
+        text-decoration: none;
+        margin: 0 10px;
+      }
 </style>
