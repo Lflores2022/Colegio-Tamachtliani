6cdfb8d281bde8b7c07f31153156a67912e57dfe
Lflores2022 <lflores@comercialmm.com>
2023-10-23 20:11:06 -0600

    upload

--- website/src/components/Ubication.vue
+++ website/src/components/Ubication.vue
@@ -41,15 +41,6 @@
 
 </section>
 
-    <footer class="footer">
-      <img class="footer-logo" src="logo.png" alt="Logo de la empresa"> Colegio Tlamachtiani
-      <div class="footer-links">
-        <a href="#">Aviso legal</a>
-        <a href="#">Política de privacidad</a>
-        <a href="#">Términos y condiciones</a>
-      </div>
-    </footer>
-
 </template>
 
 <style scoped>
@@ -100,20 +91,3 @@
 }
 
 
-.footer {
-        background-color: #000000;
-        padding: 20px;
-        text-align: center;
-        color: #fff;
-      }
-      .footer-logo {
-        width: 10px;
-      }
-    
-      .footer-links a {
-        color: #f3efef;
-        text-decoration: none;
-        margin: 0 10px;
-      }
-
-</style>
