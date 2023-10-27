276d54cb9f21ed154d7129f2fd066870998ad976
StephanieAlferezSalcido <128555593+StephanieAlferezSalcido@users.noreply.github.com>
2023-10-27 16:45:50 -0600

    index.js

--- website/src/router/index.js
+++ website/src/router/index.js
@@ -3,7 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import ProgramView from '../views/ProgramView.vue'
 import FacilitiesView from '../views/FacilitiesView.vue'
 import FormView from '../views/FormView.vue'
-import UbicationView from '../views/UbicationView.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -28,11 +28,7 @@ const router = createRouter({
       name: 'contacto',
       component: FormView
     },
-    {
-    path: '/ubicacion',
-    name: 'ubicacion',
-    component: UbicationView
-  }
+
   ]
 })
 
